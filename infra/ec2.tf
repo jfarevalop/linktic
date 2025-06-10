ae6d069f299552374b72a941684f4caa3f607fbe
jfarevalop <jfarevalop@gmail.com>
2025-06-09 22:58:55 -0500

    deploy

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
-    aws ={
-        source = "hashicorp/aws"
-        version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"  
+  region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164"  
-  instance_type = "t2.micro"               
+  ami           = "ami-011899242bb902164"
+  instance_type = "t2.micro"
 }
