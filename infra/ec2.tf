beafdf9b4c66c6fbed9adb02487ffa763bad0a09
jfarevalop <jfarevalop@gmail.com>
2025-06-09 23:29:22 -0500

    deploy

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -15,3 +15,7 @@ resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164"
   instance_type = "t2.micro"
 }
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
